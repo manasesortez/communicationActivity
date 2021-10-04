06d353ef145baa5ae49aad8c46eee3a08d74b0a9
manasesortez <manasesortez1234@hotmail.com>
2021-10-03 19:12:40 -0600

    Activitys Comunnication

--- app/src/main/java/net/lrivas/ventanasandroid/ActividadHija.java
+++ app/src/main/java/net/lrivas/ventanasandroid/ActividadHija.java
@@ -2,7 +2,7 @@ package net.lrivas.ventanasandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,34 +10,48 @@ import android.widget.TextView;
 
 public class ActividadHija extends AppCompatActivity {
 
+    View view;
     Button boton02;
     TextView resultado;
+    TextView finalLastName;
+    TextView finalAge;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad_hija);
 
+        view = this.getWindow().getDecorView();
+        view.setBackgroundResource(R.color.purple_700);
+
         resultado = findViewById(R.id.lblResultados);
+        finalLastName = findViewById(R.id.lblResultados2);
+        finalAge = findViewById(R.id.lblResultados3);
+
         boton02 = findViewById(R.id.btnRegresar);
-        boton02.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Regresar
-                //Intent ventana = new Intent(ActividadHija.this, MainActivity.class);
-                //startActivity(ventana);
-                finish();
-            }
+        boton02.setOnClickListener(v -> {
+            //Regresar
+            //Intent ventana = new Intent(ActividadHija.this, MainActivity.class);
+            //startActivity(ventana);
+            finish();
         });
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onResume() {
         super.onResume();
         Bundle valores = getIntent().getExtras();
         if(valores!=null){
             //Recuperar los datos
-            String dato = valores.getString("manzanas");
-            resultado.setText("Valor: "+ dato);
+            String nombre = valores.getString("nombre");
+            String apellido = valores.getString("apellido");
+            String edad = valores.getString("edad");
+
+            resultado.setText("Nombre: "+ nombre);
+            finalLastName.setText("Apellido: " + apellido);
+            finalAge.setText("Edad: " + edad);
+
         }
     }
 }
--- app/src/main/java/net/lrivas/ventanasandroid/MainActivity.java
+++ app/src/main/java/net/lrivas/ventanasandroid/MainActivity.java
@@ -4,30 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     Button boton01;
     EditText caja;
+    EditText apellido;
+    EditText edad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        apellido = findViewById(R.id.txtApellido);
+        edad = findViewById(R.id.txtEdad);
         caja = findViewById(R.id.txtNombre);
 
         boton01 = findViewById(R.id.btnSiguiente);
-        boton01.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Invocar la segunda activity
-                Intent miVentana = new Intent(MainActivity.this, ActividadHija.class);
-                miVentana.putExtra("manzanas",caja.getText().toString());
-                startActivity(miVentana);
-            }
+        boton01.setOnClickListener(v -> {
+            //Invocar la segunda activity
+            Intent miVentana = new Intent(MainActivity.this, ActividadHija.class);
+            miVentana.putExtra("nombre",caja.getText().toString());
+            miVentana.putExtra("edad", edad.getText().toString());
+            miVentana.putExtra("apellido", apellido.getText().toString());
+            startActivity(miVentana);
         });
     }
 }
